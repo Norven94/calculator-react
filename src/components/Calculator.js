7ba6cb7e8f1d7bbb025cc9b4f16456c20b8545d0
Oskar Anderberg <otto.anderberg@gmail.com>
2021-02-16 13:45:27 +0100

    Added calculator to home

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,7 +3,7 @@ import { calculator } from "../css/Calculator.module.css"
 export default function Calculator () {
     return (
         <div className={calculator}>
-            
+            <h1>This is the calculator component</h1>
         </div>
     )
 }
