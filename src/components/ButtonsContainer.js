c003741763673c1c92f222868553ac4ee5e9f80a
Oskar Anderberg <otto.anderberg@gmail.com>
2021-02-16 14:26:29 +0100

    Created a buttons container

---
+++ src/components/ButtonsContainer.js
@@ -0,0 +1,9 @@
+import { buttons } from "../css/ButtonsContainer.module.css"
+
+export default function ButtonsContainer () {
+    return (
+        <div className={buttons}>
+            <p>Here we will map buttons later.</p>
+        </div>
+    )
+}
